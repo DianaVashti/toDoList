fc82245494b2319cc3ee4fdf5b169578ee72c06f
FrankieFabuloso <frocha@ucsc.edu>
2016-12-13 09:53:51 -0800

    adds addTodo full functionality; adds getAll functionality to retrive data from db

--- database/db.js
+++ database/db.js
@@ -1,9 +1,34 @@
-const _=require('lodash')
+const _ = require( 'lodash' )
 
-const fs =require('fs')
+const fs =require( 'fs' )
 
-if(fs.existSync('.env')){
-  require('dotenv').config()
+if( fs.existsSync( '.env' ) ){
+  require( 'dotenv' ).config()
 }
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL
+
+const pgp = require( 'pg-promise' )()
+
+const db = pgp( connectionString )
+
+// - - - QUERIES - - -
+const INSERT_TODO = 'INSERT INTO todos(name, description) VALUES($1, $2) ON CONFLICT DO NOTHING RETURNING *'
+
+const GET_ALL_TODOS = 'SELECT * FROM todos'
+
+const Todos = {
+  addTodo: ( todo ) => {
+    const {name, description} = todo
+    console.log("In DB.js: ")
+    console.log(name)
+    console.log(description)
+    return db.one( INSERT_TODO, [name, description])
+      .then( result => {
+        console.log( result )
+      })
+  },
+  getAll: () => db.any( GET_ALL_TODOS, [] )
+}
+
+module.exports = {Todos};
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const Todos = require('../database/db').Todos
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.render('index', { title: 'Express' })
+})
 
-router.get('/fart', function(req, res, next) {
-  res.render('index', { title: 'this is poop' });
-});
+router.post('/add', function(req, res, next) {
+  const todoItem = req.body
+  console.log("In routes", todoItem)
+  Todos.addTodo( todoItem )
+    .then( result => {
+      res.redirect('/')
+    })
+})
 
-// router.post('/add', function(req, res, next) {
-//
-// }
+router.get('/list', function(req, res, next){
+  Todos.getAll()
+    .then( results => {
+      console.log(results);
+    })
+})
 
 module.exports = router;

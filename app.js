94ba11175cfdd5260a98d6ff348b380809d1ed91
Diana Vashti <diana.vashti@gmail.com>
2016-12-16 17:55:23 -0800

    fixed all the mistakes branch name is a lie

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-// var passport = require('passport');
-// var LocalStrategy = require('passport-local').Strategy;
-// var session = require('express-session')
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -17,23 +14,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(passport.initialize());
-// app.use(passport.session());
+
 app.use('/', index);
 app.use('/users', users);
 
-//passport config
-// var Account = require('./database/account');
-// passport.use(new LocalStrategy(Account.authenticate()));
-// passport.serializeUser(Account.serializeUser());
-// passport.deserializeUser(Account.deserializeUser());
 
 
 // catch 404 and forward to error handler
